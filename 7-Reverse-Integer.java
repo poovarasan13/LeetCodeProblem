92cfafd880d11d8efaf258e8494de332ae6fb9be
POOVARASAN S <717822f239@kce.ac.in>
2024-12-20 15:13:35 +0530

    [7] [Time Beats: 100.00%] [Memory Beats: 70.03%] - LeetPush

--- 7-Reverse-Integer.java
+++ 7-Reverse-Integer.java
@@ -1,21 +1,14 @@
 class Solution {
     public int reverse(int x) {
-       
-        long rem=0,rev=0;
-          
-          int flag=0;
-    if(x<0) {
-        x=x*(-1);
-        flag=1;
-    }     while(x>0){
-        
+ 
+        long rem=0,rev=0; 
+          int flag=0;   
+         while(x!=0){   
             rem=x%10;
             rev=rev*10 +rem;
             x=x/10;
         }
-        if(rev>Integer.MAX_VALUE|| rev<Integer.MIN_VALUE) return 0;
-        if(flag==1) return (int)-rev;
-         
+        if(rev>Integer.MAX_VALUE|| rev<Integer.MIN_VALUE) return 0;   
          return (int)rev;
        
     }
