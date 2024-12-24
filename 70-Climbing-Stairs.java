e507d841be40f09b0defe56889c92a837b962833
POOVARASAN S <717822f239@kce.ac.in>
2024-12-24 10:21:20 +0530

    [70] [Time Beats: 100.00%] [Memory Beats: 11.95%] - LeetPush

---
+++ 70-Climbing-Stairs.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int climbStairs(int n) {
+        int [] dp=new int[n+1];
+        return climb(n,dp);
+        
+    }
+    public int climb(int n,int [] dp)
+    {
+               if( n<=1 ) return 1;
+               if(dp[n]!=0) return dp[n];
+             
+               dp[n]= climb(n-1,dp)+climb(n-2,dp);
+               return dp[n];
+    }
+    
+
+}
