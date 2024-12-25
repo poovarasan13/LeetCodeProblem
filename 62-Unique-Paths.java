1f93dd4106974a36a271fef5a56ae536b0b962c0
POOVARASAN S <717822f239@kce.ac.in>
2024-12-25 12:04:45 +0530

    [62] [Time Beats: 100.00%] [Memory Beats: 11.41%] - LeetPush

--- 62-Unique-Paths.java
+++ 62-Unique-Paths.java
@@ -1,20 +1,21 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int dp[][]=new int[m][n];
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                dp[i][j]=1;
-            }
+        int dp[][]=new int[n][m];
+        for(int [] a:dp){
+            Arrays.fill(a,-1);
         }
-        for(int i=1;i<m;i++)
-        {
-            for(int j=1;j<n;j++)
-            {
-                dp[i][j]=dp[i-1][j]+dp[i][j-1];
-            }
-        }
-        return dp[m-1][n-1];  
-}
+
+        return helper(n-1,m-1,dp);
+    }
+    public int helper(int i,int j, int [][] dp)
+    {
+       if(i==0 || j==0 ) return 1;
+       if(dp[i][j]!=-1) return dp[i][j];
+
+       int x= helper(i-1,j,dp);
+       int y=helper(i,j-1,dp);
+       return dp[i][j]=x+y;
+    }
+       
+
 }
