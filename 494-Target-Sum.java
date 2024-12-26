d53c646784274e42c5491daf2cc9f29923ca883c
POOVARASAN S <717822f239@kce.ac.in>
2024-12-26 14:46:50 +0530

    [494] [Time Beats: 11.84%] [Memory Beats: 60.07%] - LeetPush

--- 494-Target-Sum.java
+++ 494-Target-Sum.java
@@ -1,16 +1,15 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        int [] dp=new int [nums.length+1];
-       return helper(nums,(nums.length)-1,target,0,dp);
+       return helper(nums,(nums.length)-1,target,0);
     }
-    public int helper(int[] nums,int i,int target,int sum,int [] dp){
+    public int helper(int[] nums,int i,int target,int sum){
    if(i<0) 
    {
     return target==sum ? 1:0;
    }
-    int a=helper(nums,i-1,target,sum+nums[i],dp);
-     int b=helper(nums,i-1,target,sum-nums[i],dp);
-     dp[i]=a+b;
-    return dp[i];
+   
+    int a=helper(nums,i-1,target,sum+nums[i]);
+     int b=helper(nums,i-1,target,sum-nums[i]);
+    return a+b;
     }
 }
