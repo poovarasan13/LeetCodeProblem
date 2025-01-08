d662cc12b3802c1eb87a3ac0feefa4179eb09d37
POOVARASAN S <717822f239@kce.ac.in>
2025-01-08 15:45:15 +0530

    [283] [Time Beats: 99.80%] [Memory Beats: 40.04%] - LeetPush

---
+++ 283-Move-Zeroes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int l=nums.length,j=0,i;
+        int [] array=new int[l];
+        for(i=0;i<l;i++) {
+            if(nums[i]!=0) array[j++]=nums[i];
+        }
+
+   int p=0;
+        for(int a:array)
+        {
+            nums[p++]=a;
+        }
+              
+    }
+}
