9e8fb544e54b32da06501ddc046700038461239c
POOVARASAN S <717822f239@kce.ac.in>
2025-01-13 07:53:07 +0530

    [3223] [Time Beats: 82.12%] [Memory Beats: 53.64%] - LeetPush

--- 3223-Minimum-Length-of-String-After-Operations.java
+++ 3223-Minimum-Length-of-String-After-Operations.java
@@ -3,18 +3,22 @@ class Solution {
         HashMap<Character,Integer> map=new HashMap<>();
         char[] ch=s.toCharArray();
         int n=ch.length;
+        int[] freq=new int[26];
         for( char c:ch)
         {
-            map.put(c,map.getOrDefault(c,0)+1);
+            // map.put(c,map.getOrDefault(c,0)+1);
+            freq[c-'a']++;
         }
         int answer=0;
-        for(Map.Entry<Character,Integer>m: map.entrySet())
+        for(int i=0;i<26;i++)
         {
-            int val=m.getValue();
-            if(val%2==0)
-               answer+=2;
-            else 
-               answer+=1;
+            if(freq[i]!=0)
+            {
+                if(freq[i]%2==0)
+                    answer+=2;
+                else 
+                    answer+=1;
+            }
         }
         return answer;
     }
