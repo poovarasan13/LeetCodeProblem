4e243a1d74b084160961c4de6005a90bf08245ae
POOVARASAN S <717822f239@kce.ac.in>
2025-03-05 08:42:18 +0530

    [2579] [Time Beats: 100.00%] [Memory Beats: 73.36%] - LeetPush

--- Count-Total-Number-of-Colored-Cells.java
+++ Count-Total-Number-of-Colored-Cells.java
@@ -2,7 +2,7 @@ class Solution {
     public long coloredCells(int n) {
         if(n==1)
          return 1;
-         long ans=((long)n*n +(long)(n-1 )*(n-1));
-        return ans;
+         
+        return ((long)n*n +(long)(n-1 )*(n-1));
     }
 }
