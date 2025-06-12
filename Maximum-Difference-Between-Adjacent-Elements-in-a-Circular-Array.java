d52407ced9dab2d78d6be78a3599bdf968d5cd8f
Poovarasan S <poovarasan936161@gmail.com>
2025-06-12 11:30:13 +0530

    [3423] [Time Beats: 98.36%] [Memory Beats: 52.79%] - LeetPush

---
+++ Maximum-Difference-Between-Adjacent-Elements-in-a-Circular-Array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxAdjacentDistance(int[] nums) {
+      int n=nums.length;
+      int max=Math.abs(nums[0]-nums[n-1]);
+      for(int i=1;i<n;i++)
+      {
+        max=Math.max(max,Math.abs(nums[i-1]-nums[i]));
+      }  
+      return max;
+    }
+}
