a2f216cbf13f499a6dd8bfc318ba7319018c1881
Poovarasan S <poovarasan936161@gmail.com>
2025-08-24 21:00:10 +0530

    [696] [Time Beats: 88.63%] [Memory Beats: 16.44%] - LeetPush

---
+++ Count-Binary-Substrings.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countBinarySubstrings(String s) {
+        int prev=0,curr=1,result=0;
+        for(int i=1;i<s.length();i++)
+        {
+            if(s.charAt(i)==s.charAt(i-1)) curr++;
+            else{
+                result+=Math.min(curr,prev);
+                prev=curr;
+                curr=1;
+            }
+        }
+        result+=Math.min(curr,prev);
+        return result;
+    }
+}
